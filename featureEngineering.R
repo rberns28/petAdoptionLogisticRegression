6401d70e83086d4d10b54d95cd7cb92cbdd4bdbc
Ryan Berns <rberns28@gmail.com>
2017-11-12 10:01:11 -0500

    Added functions to calculate model statistics and another function to transform coefficients to be more interpretable

--- featureEngineering.R
+++ featureEngineering.R
@@ -74,4 +74,75 @@ base <- base %>% mutate(month=format(DateTime, "%m"))
 
 #---------------------- Count Nulls ----------------------#
 apply(base, 2, function(x) print(sum(is.na(x))))
-nrow(na.omit(base[,-2]))
+base <- na.omit(base[,-2])
+
+#---------------------- Plotting -------------------------#
+
+factorPlot <-function(df,dataCol) {
+df %>% select(OutcomeType,as.factor(dataCol))  %>% 
+  ggplot(aes(x=dataCol, fill=OutcomeType)) +
+  geom_bar(position = "fill") +
+  labs(x = "Feature", y= "Outcome") + 
+  coord_flip()
+}
+
+ggplot(base,aes(OutcomeType, daysOld)) +
+  geom_boxplot()
+
+library(ggthemes)
+library(scales)
+ggplot(base, aes(OutcomeType, daysOld)) +
+  geom_boxplot() + 
+  ggtitle("Outcome by Days old") +
+  theme_tufte() + 
+  scale_y_continuous(labels = comma)
+
+ggplot(base, aes(OutcomeType, as.numeric(hour))) +
+  geom_boxplot() + 
+  ggtitle("Outcome by Hour") +
+  theme_tufte() + 
+  scale_y_continuous(labels = comma)
+
+#---------------------- Check for Collinearity -------------------------#
+
+
+#---------------------- Modeling -------------------------#
+
+# Split into Train and Test
+## 80% of the sample size
+drops <- c('AnimalID','DateTime','SexuponOutcome','AgeuponOutcome','Breed','Color','numericPart','daysOld','SpayedIndicator')
+base_tmp <- base[ , !(names(base) %in% drops)]
+
+smp_size <- floor(0.80 * nrow(base_tmp))
+set.seed(123)
+train_ind <- sample(seq_len(nrow(base_tmp)), size = smp_size)
+train <- base_tmp[train_ind, ]
+test <- base_tmp[-train_ind, ]
+
+# Baseline model
+glm1 <- glm(OutcomeType ~ . , data=train, family = binomial())
+summary(glm1)
+
+# Extract more interpretable coefficients and create a function to do it
+formatCf <- function(glm_model) {
+  for(i in glm_model$coefficients) 
+    {
+    print((exp(i) - 1)*100)
+}}
+
+formatCf(glm1)
+
+# Extract model summary statistics and save model results to a dataframe 
+modelSummary <-function(dataset,model,description) {
+  predict.class<- round(predict(model,newdata= dataset, type="response"), digits = 0)
+  conf <- xtabs(~ dataset$OutcomeType + predict.class)
+  accuracy <- (round((conf[1,1] + conf[2,2])/sum(conf),4)*100)
+  recall <- (round(conf[2,2]/sum(conf[2,]),4)*100)
+  specificity <- (round(conf[1,1]/sum(conf[,1]),4)*100)
+  precision <- (round(conf[2,2]/sum(conf[,2]),4)*100)
+  newLine <- c(description,accuracy,recall,specificity,precision)
+  ifelse(exists("model_results") && is.data.frame(get("model_results")),
+         model_results <- rbind(model_results,newLine),
+         model_results <- data.frame(description,accuracy,recall,specificity,precision,stringsAsFactors=FALSE))
+  model_results <<- model_results
+}
